7b4ee3ad61122eef7b962bcdb788545ce919642b
TheNelud <riwok@bk.ru>
2023-04-18 14:10:06 +0300

    6 приложений в одном

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import Counter from "./pages/Counter";
+// import Modal from "./pages/Modal";
+// import Quiz from "./pages/Quiz";
+
+import Users from "./pages/Users";
+
+
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Counter/> */}
+        {/* <Modal/> */}
+        {/* <Quiz/> */}
+        <Users/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Users.js
@@ -0,0 +1,10 @@
+const Users = () => {
+    return ( 
+    <div className="UsersApp">
+      <Users />
+      {/* <Success /> */}
+    </div>
+     );
+}
+ 
+export default Users;
